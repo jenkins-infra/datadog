d2a1b8c9a8afb87b03b00627d80550d4a708621b
Olblak <me@olblak.com>
2019-08-23 10:06:11 +0200

    Add Synthetics checks for pkg.jenkins.io

---
+++ plans/synthetics_pkg.tf
@@ -0,0 +1,68 @@
+resource "datadog_synthetics_test" "pkg_jenkins_io" {
+  type = "browser"
+  request {
+    method = "GET"
+    url = "https://pkg.jenkins.io"
+  }
+  assertions = [
+    {
+      type = "statusCode"
+      operator = "is"
+      target = "200"
+    }
+  ]
+  locations = [ "aws:eu-central-1" ]
+  options {
+    tick_every = 900
+  }
+  name = "pkg.jenkins.io"
+  message = "Notify @pagerduty"
+  tags = [
+    "jenkins.io",
+    "production"
+  ]
+
+  device_ids = [
+    "laptop_large",
+    "tablet",
+    "mobile_small"
+
+  ]
+
+  status = "live"
+}
+
+## Do not monitor https
+resource "datadog_synthetics_test" "pkg_jenkinsci_org" {
+  type = "browser"
+  request {
+    method = "GET"
+    url = "http://pkg.jenkins-ci.org"
+  }
+  assertions = [
+    {
+      type = "statusCode"
+      operator = "is"
+      target = "200"
+    }
+  ]
+  locations = [ "aws:eu-central-1" ]
+  options {
+    tick_every = 900
+  }
+  name = "pkg.jenkins-ci.org"
+  message = "Notify @pagerduty"
+  tags = [
+    "jenkins-ci.org",
+    "production"
+  ]
+
+  device_ids = [
+    "laptop_large",
+    "tablet",
+    "mobile_small"
+
+  ]
+
+  status = "live"
+}
