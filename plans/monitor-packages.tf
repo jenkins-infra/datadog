ea08b518881904373cbd980731aa4cb5b89484db
Olivier Vernin <olivier@vernin.me>
2020-12-11 16:00:53 +0100

    Fix typo
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- plans/monitor-packages.tf
+++ plans/monitor-packages.tf
@@ -4,7 +4,7 @@ resource "datadog_monitor" "distribution_package_can_be_downloaded" {
   message             = "{{#is_alert}}\n\n- Please ask help on IRC channel #jenkins-release and #jenkins-infra\n- If appropriate, please open an incident on our [status page](https://github.com/jenkins-infra/status)\n\n{{/is_alert}}\n\n{{#is_recovery}}\n\n- Don't forget  to close an open incidents on status.jenkins.io\n- Have you update our [runbooks][https://github.com/jenkins-infra/runbooks]? \n\n{{/is_recovery}}\n\nNotify: @pagerduty"
   query               = "avg(last_30m):avg:jenkins.package.available{*} by {package} <= 0"
   notify_audit        = false
-  timemout_h          = 0
+  timeout_h           = 0
   locked              = false
   no_data_timeframe   = 60
   renotify_interval   = 5 
