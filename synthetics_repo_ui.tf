ab60d4edf16a25ea723a30214cb691ece9a700bd
smerle33 <smerle@cloudbees.com>
2022-05-06 16:28:51 +0200

    chore(repowebui): correct the probe

--- synthetics_repo_ui.tf
+++ synthetics_repo_ui.tf
@@ -13,7 +13,7 @@ resource "datadog_synthetics_test" "repojenkinsciorgui" {
   options_list {
     tick_every = 900
   }
-  name    = "repo.jenkins-ci.org"
+  name    = "repo.jenkins-ci.org (UI)"
   message = "Notify @pagerduty"
   tags    = ["production", "jenkins-ci.org"]
 
@@ -24,48 +24,4 @@ resource "datadog_synthetics_test" "repojenkinsciorgui" {
     "tablet",
     "mobile_small"
   ]
-}
-
-resource "datadog_synthetics_test" "repojenkinsciorg" {
-  type = "browser"
-  request_definition {
-    method = "GET"
-    url    = "https://repo.jenkins-ci.org"
-  }
-  assertion {
-    type     = "statusCode"
-    operator = "is"
-    target   = "301"
-  }
-  assertion {
-    type     = "header"
-    operator = "is"
-    target   = "location: https://repo.jenkins-ci.org/ui"
-
-  }
-  locations = ["aws:eu-central-1"]
-  # Can't work at the moment according datadog message
-  # Synthetics is only available for the Datadog US site.
-  # locations = [
-  #   "aws:eu-central-1",
-  #   "aws:eu-central-2",
-  #   "aws:us-east-2",
-  #   "aws:us-west-2",
-  #   "aws:ap-northeast-1",
-  #   "aws:ap-southeast-2"
-  # ]
-  options_list {
-    tick_every = 900
-  }
-  name    = "repo.jenkins-ci.org"
-  message = "Notify @pagerduty"
-  tags    = ["production", "jenkins-ci.org"]
-
-  device_ids = [
-    "laptop_large",
-    "tablet",
-    "mobile_small"
-  ]
-
-  status = "live"
 }
