0757268690657dcd2e6f917522fb1deb6c344e33
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-10-26 10:33:31 +0200

    feat: add synthetic monitoring on artifact-caching-proxy providers (#113)

---
+++ synthetics_artifact-caching-proxy-providers.tf
@@ -0,0 +1,25 @@
+resource "datadog_synthetics_test" "artifact-caching-proxy-providers" {
+  for_each = toset(var.artifact_caching_proxy_providers)
+  type     = "api"
+  request_definition {
+    method = "GET"
+    url    = "https://repo.${each.value}.jenkins.io/healthz"
+  }
+  assertion {
+    type     = "statusCode"
+    operator = "is"
+    target   = "200"
+  }
+  locations = ["aws:eu-central-1"]
+  options_list {
+    tick_every = 900
+  }
+  name    = "repo.${each.value}.jenkins.io"
+  message = "Notify @pagerduty"
+  tags = [
+    "jenkins.io",
+    "production"
+  ]
+
+  status = "live"
+}
