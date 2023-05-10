379ea98857ee7895b76d5fdc35d37e32dd2a54a1
Damien Duportal <damien.duportal@gmail.com>
2023-05-10 17:03:38 +0200

    feat(disk monitor) utilizes the system.disk.in_use fraction to simplify formula and avoid false positives
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- synthetics_jenkinsio.tf
+++ synthetics_jenkinsio.tf
@@ -12,7 +12,7 @@ resource "datadog_synthetics_test" "jenkinsio" {
   locations = ["aws:eu-central-1"]
   options_list {
     tick_every = 900
-    
+
     retry {
       count    = 2
       interval = 300
