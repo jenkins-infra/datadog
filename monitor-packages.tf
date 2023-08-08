469afe3bbefe28a8d197e2c8f6fcd1e40553378c
Alexander Brandes <brandes.alexander@web.de>
2023-08-08 18:04:30 +0200

    Replace jenkins-release with matrix room (#212)
    * Replace jenkins-release with matrix room
    
    * Update monitor-packages.tf

--- monitor-packages.tf
+++ monitor-packages.tf
@@ -1,7 +1,7 @@
 resource "datadog_monitor" "distribution_package_can_be_downloaded" {
   name                = "Package {{ package.name }} is not available from get.jenkins.io"
   type                = "query alert"
-  message             = "{{#is_alert}}\n\n- Please ask help on IRC channel #jenkins-release and #jenkins-infra\n- If appropriate, please open an incident on our [status page](https://github.com/jenkins-infra/status)\n\n{{/is_alert}}\n\n{{#is_recovery}}\n\n- Don't forget  to close an open incidents on status.jenkins.io\n- Have you update our [runbooks][https://github.com/jenkins-infra/runbooks]? \n\n{{/is_recovery}}\n\nNotify: @pagerduty"
+  message             = "{{#is_alert}}\n\n- Please ask for help in the Matrix channels #jenkins-release:matrix.org and #jenkins-infra:matrix.org\n- If appropriate, please open an incident on our [status page](https://github.com/jenkins-infra/status)\n\n{{/is_alert}}\n\n{{#is_recovery}}\n\n- Don't forget  to close an open incidents on status.jenkins.io\n- Have you update our [runbooks][https://github.com/jenkins-infra/runbooks]? \n\n{{/is_recovery}}\n\nNotify: @pagerduty"
   query               = "avg(last_30m):avg:jenkins.package.available{*} by {package} <= 0"
   notify_audit        = false
   timeout_h           = 0
