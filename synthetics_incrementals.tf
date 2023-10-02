7c98c92a1a9fb2fb4f6ea6f904400ffdc6bc1bbb
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2023-10-02 12:10:17 +0200

    Update synthetics_incrementals.tf
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- synthetics_incrementals.tf
+++ synthetics_incrementals.tf
@@ -7,6 +7,9 @@ resource "datadog_synthetics_test" "incrementalsjenkinsio" {
   assertion {
     type     = "statusCode"
     operator = "is"
+    # TODO: improve healthcheck on the incremental publisher app
+    ## Checking for an HTTP/404 covers some errors (all HTTP/5xx such as no pod running) but not all cases
+    ## Still better than no monitoring at all
     target   = "404"
   }
   locations = ["aws:eu-central-1"]
