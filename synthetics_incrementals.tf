4147e724466f7cd917703030c4b0779de8bc494f
Damien Duportal <damien.duportal@gmail.com>
2024-02-12 09:42:48 +0100

    Update synthetics_incrementals.tf

--- synthetics_incrementals.tf
+++ synthetics_incrementals.tf
@@ -2,6 +2,7 @@ resource "datadog_synthetics_test" "incrementalsjenkinsio" {
   type = "api"
   request_definition {
     method = "GET"
+    # https://github.com/jenkins-infra/incrementals-publisher/blob/a754f43d44be5f6b09e2ac3f9e5600e04175936b/index.js#L62
     url    = "https://incrementals.jenkins.io/readiness"
   }
   assertion {
