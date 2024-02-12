b4460de01e9df951594b1d38ebc8a13c09f3c463
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2024-02-12 09:45:31 +0100

    fix(incrementals): use `/readiness` endpoint returning a 200 status code (#243)
    * fix(incrementals): use `readiness` URL returning 200 status code
    
    * Update synthetics_incrementals.tf
    
    ---------
    
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- synthetics_incrementals.tf
+++ synthetics_incrementals.tf
@@ -2,15 +2,13 @@ resource "datadog_synthetics_test" "incrementalsjenkinsio" {
   type = "api"
   request_definition {
     method = "GET"
-    url    = "https://incrementals.jenkins.io/"
+    # https://github.com/jenkins-infra/incrementals-publisher/blob/a754f43d44be5f6b09e2ac3f9e5600e04175936b/index.js#L62
+    url    = "https://incrementals.jenkins.io/readiness"
   }
   assertion {
     type     = "statusCode"
     operator = "is"
-    # TODO: improve healthcheck on the incremental publisher app
-    ## Checking for an HTTP/404 covers some errors (all HTTP/5xx such as no pod running) but not all cases
-    ## Still better than no monitoring at all
-    target   = "404"
+    target   = "200"
   }
   locations = ["aws:eu-central-1"]
   options_list {
