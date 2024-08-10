f9c9c67dbcd96fcaffb9db5a0d4ea1cd6a436589
Damien Duportal <damien.duportal@gmail.com>
2024-08-10 12:09:43 +0200

    cleanup(acp) remove (public) ACP monitoring (#257)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
 variable "datadog_api_key" {}
 variable "datadog_app_key" {}
 variable "datadog_jenkinsuser_password" {}
-variable "artifact_caching_proxy_providers" {
-  description = "Available artifact-caching-proxy providers"
-  type        = list(string)
-  default     = ["azure"]
-}
