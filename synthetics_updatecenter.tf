457ce441a4e4d9c5f1c0434a95a56b3d80d7a645
Damien Duportal <damien.duportal@gmail.com>
2024-09-26 08:55:14 +0200

    feat(synt/updates.jenkins.io) create a matrix of URLs to check (#266)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- synthetics_updatecenter.tf
+++ synthetics_updatecenter.tf
@@ -1,34 +1,36 @@
-resource "datadog_synthetics_test" "updates_jenkins_io" {
-  type = "api"
-  request_definition {
-    method = "GET"
-    url    = "https://updates.jenkins.io"
-  }
-  assertion {
-    type     = "statusCode"
-    operator = "is"
-    target   = "200"
-  }
-  locations = ["aws:eu-central-1"]
-  options_list {
-    tick_every       = 900
-    follow_redirects = true
-  }
-  name    = "updates.jenkins.io"
-  message = "Notify @pagerduty"
-  tags = [
-    "jenkins.io",
-    "production"
+locals {
+  update_center_scheme = ["https", "http"]
+  update_center_hosts  = ["updates.jenkins.io", "updates.jenkins-ci.org"]
+  update_center_uris = [
+    "/",
+    "/index.html",
+    "/update-center.json",
+    "/current/update-center.json",
+    "/latest/zos-connector.hpi",
+    "/download/plugins/zos-connector/3.257.v41e144167971/zos-connector.hpi",
+    "/update-center.json?id=default&version=2.462.2",
   ]
 
-  status = "live"
+  update_center_urls = [
+    for triplet in setproduct(local.update_center_scheme, local.update_center_hosts, local.update_center_uris) : {
+      scheme   = triplet[0]
+      hostname = triplet[1]
+      uri      = triplet[2]
+    }
+  ]
+
 }
 
-resource "datadog_synthetics_test" "updates_jenkinsci_org" {
+resource "datadog_synthetics_test" "updates_center" {
+  for_each = tomap({
+    for url in local.update_center_urls : format("%s://%s%s", url.scheme, url.hostname, url.uri) => url.hostname
+  })
+
+
   type = "api"
   request_definition {
     method = "GET"
-    url    = "https://updates.jenkins-ci.org"
+    url    = each.key
   }
   assertion {
     type     = "statusCode"
@@ -40,10 +42,10 @@ resource "datadog_synthetics_test" "updates_jenkinsci_org" {
     tick_every       = 900
     follow_redirects = true
   }
-  name    = "updates.jenkins-ci.org"
+  name    = each.key
   message = "Notify @pagerduty"
   tags = [
-    "jenkins-ci.org",
+    each.value,
     "production"
   ]
 
