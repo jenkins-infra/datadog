ba414f5ffefed91b97c7ebacd4e3b56d2c1d9ec5
Damien Duportal <damien.duportal@gmail.com>
2025-03-19 18:32:21 +0100

    Delete imports.tf

--- imports.tf
+++
@@ -1,8 +0,0 @@
-import {
-  id = "yy7n1F4xREmOKrs-SeSOYw"
-  to = datadog_logs_integration_pipeline.keycloak
-}
-import {
-  id = "VbeyK5ssQNSsJhPbl7srGw"
-  to = datadog_logs_integration_pipeline.falco
-}
