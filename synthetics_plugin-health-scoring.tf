a7c15b61fdb4baf9ca61abdac98a13a2272d339a
Damien Duportal <damien.duportal@gmail.com>
2025-04-28 13:16:39 +0200

    feat(synthetics/PHS) use the correct health probe

--- synthetics_plugin-health-scoring.tf
+++ synthetics_plugin-health-scoring.tf
@@ -2,43 +2,27 @@ resource "datadog_synthetics_test" "plugin_health_scoring" {
   type = "api"
   request_definition {
     method = "GET"
-    url    = "https://plugin-health.jenkins.io"
+    url    = "https://plugin-health.jenkins.io/actuator/health"
   }
   assertion {
     type     = "statusCode"
     operator = "is"
     target   = "200"
+  }
+  assertion {
+    type = "body"
+    operator = "validatesJSONPath"
+    targetjsonpath {
+      jsonpath    = "$.status"      
+      operator    = "is"
+      targetvalue = "UP"
+    }
   }
   locations = ["aws:eu-central-1"]
   options_list {
     tick_every = 900
   }
   name    = "plugin-health.jenkins.io"
-  message = "Notify @pagerduty"
-  tags = [
-    "jenkins.io",
-    "production"
-  ]
-
-  status = "live"
-}
-
-resource "datadog_synthetics_test" "plugin_health_scoring_probes" {
-  type = "api"
-  request_definition {
-    method = "GET"
-    url    = "https://plugin-health.jenkins.io/probes"
-  }
-  assertion {
-    type     = "statusCode"
-    operator = "is"
-    target   = "200"
-  }
-  locations = ["aws:eu-central-1"]
-  options_list {
-    tick_every = 900
-  }
-  name    = "plugin-health.jenkins.io-probes"
   message = "Notify @pagerduty"
   tags = [
     "jenkins.io",
