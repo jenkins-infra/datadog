1e363d84e6aaeda16e333f8249f3055a3b0d5e31
Damien Duportal <damien.duportal@gmail.com>
2025-08-06 08:47:57 +0200

    feat(synthetics/updates.jio) disable update center monitoring for operation https://github.com/jenkins-infra/helpdesk/issues/4758
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- synthetics_updatecenter.tf
+++ synthetics_updatecenter.tf
@@ -1,62 +1,62 @@
-locals {
-  update_center_scheme = ["https", "http"]
-  update_center_hosts  = ["updates.jenkins.io", "updates.jenkins-ci.org"]
-  update_center_uris = [
-    "/",
-    "/index.html",
-    "/update-center.json",
-    "/current",
-    "/current/",
-    "/current/update-center.json",
-    "/latest/zos-connector.hpi",
-    "/download/plugins/zos-connector/3.257.v41e144167971/zos-connector.hpi",
-    "/update-center.json?id=default&version=2.462.2",
-    "/updates",
-    "/updates/",
-    "/updates/hudson.tasks.Maven.MavenInstaller.json",
-    "/current/updates",
-    "/current/updates/",
-    "/latest",
-    "/latest/",
-  ]
+# locals {
+#   update_center_scheme = ["https", "http"]
+#   update_center_hosts  = ["updates.jenkins.io", "updates.jenkins-ci.org"]
+#   update_center_uris = [
+#     "/",
+#     "/index.html",
+#     "/update-center.json",
+#     "/current",
+#     "/current/",
+#     "/current/update-center.json",
+#     "/latest/zos-connector.hpi",
+#     "/download/plugins/zos-connector/3.257.v41e144167971/zos-connector.hpi",
+#     "/update-center.json?id=default&version=2.462.2",
+#     "/updates",
+#     "/updates/",
+#     "/updates/hudson.tasks.Maven.MavenInstaller.json",
+#     "/current/updates",
+#     "/current/updates/",
+#     "/latest",
+#     "/latest/",
+#   ]
 
-  update_center_urls = [
-    for triplet in setproduct(local.update_center_scheme, local.update_center_hosts, local.update_center_uris) : {
-      scheme   = triplet[0]
-      hostname = triplet[1]
-      uri      = triplet[2]
-    }
-  ]
+#   update_center_urls = [
+#     for triplet in setproduct(local.update_center_scheme, local.update_center_hosts, local.update_center_uris) : {
+#       scheme   = triplet[0]
+#       hostname = triplet[1]
+#       uri      = triplet[2]
+#     }
+#   ]
 
-}
+# }
 
-resource "datadog_synthetics_test" "updates_center" {
-  for_each = tomap({
-    for url in local.update_center_urls : format("%s://%s%s", url.scheme, url.hostname, url.uri) => url.hostname
-  })
+# resource "datadog_synthetics_test" "updates_center" {
+#   for_each = tomap({
+#     for url in local.update_center_urls : format("%s://%s%s", url.scheme, url.hostname, url.uri) => url.hostname
+#   })
 
 
-  type = "api"
-  request_definition {
-    method = "GET"
-    url    = each.key
-  }
-  assertion {
-    type     = "statusCode"
-    operator = "is"
-    target   = "200"
-  }
-  locations = ["aws:eu-central-1"]
-  options_list {
-    tick_every       = 900
-    follow_redirects = true
-  }
-  name    = each.key
-  message = "Notify @pagerduty"
-  tags = [
-    each.value,
-    "production"
-  ]
+#   type = "api"
+#   request_definition {
+#     method = "GET"
+#     url    = each.key
+#   }
+#   assertion {
+#     type     = "statusCode"
+#     operator = "is"
+#     target   = "200"
+#   }
+#   locations = ["aws:eu-central-1"]
+#   options_list {
+#     tick_every       = 900
+#     follow_redirects = true
+#   }
+#   name    = each.key
+#   message = "Notify @pagerduty"
+#   tags = [
+#     each.value,
+#     "production"
+#   ]
 
-  status = "live"
-}
+#   status = "live"
+# }
