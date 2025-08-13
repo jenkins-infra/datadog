2ca25a6f5468bf3278edec96779bd16908b5caba
Jaideep <79799016+jayfranco999@users.noreply.github.com>
2025-08-13 18:42:43 +0530

    feat(monitoring): add datadog synthetics for jenkins build status monitoring (#313)
    * feat(monitoring): add datadog synthetics for jenkins build status monitoring
    
    Signed-off-by: jayfranco999 <jaideepfrancis999@gmail.com>
    
    * Update synthetics_build-reports.tf
    
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>
    
    ---------
    
    Signed-off-by: jayfranco999 <jaideepfrancis999@gmail.com>
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ synthetics_build-reports.tf
@@ -0,0 +1,29 @@
+resource "datadog_synthetics_test" "docker_404" {
+  type = "api"
+  request_definition {
+    method = "GET"
+    url    = "https://builds.reports.jenkins.io/build_status_reports/infra.ci.jenkins.io/docker-jobs/docker-404/main/status.json"
+  }
+  assertion {
+    type     = "statusCode"
+    operator = "is"
+    target   = "200"
+  }
+  assertion {
+    type     = "body"
+    operator = "contains"
+    target   = "\"build_status\":\"SUCCESS\""
+  }
+  locations = ["aws:eu-central-1"]
+  options_list {
+    tick_every = 900
+    retry {
+      count    = 2
+      interval = 300
+    }
+  }
+  name    = "docker-404 build status"
+  # message = "Notify @pagerduty"
+  tags    = ["production", "jenkins-infra", "docker-404"]
+  status = "live"
+}
