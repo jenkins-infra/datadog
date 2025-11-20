7635cbbd4f3276e406de4b7fd6a89a339ff74fc2
Damien Duportal <damien.duportal@gmail.com>
2025-11-20 20:36:37 +0100

    Update monitor-packages.tf

--- monitor-packages.tf
+++ monitor-packages.tf
@@ -2,7 +2,7 @@ resource "datadog_monitor" "distribution_package_can_be_downloaded" {
   name                = "Package {{ package.name }} is not available from get.jenkins.io"
   type                = "query alert"
   message             = "{{#is_alert}}\n\n- Please ask for help in the Matrix channels #jenkins-release:matrix.org and #jenkins-infra:matrix.org\n- If appropriate, please open an incident on our [status page](https://github.com/jenkins-infra/status)\n\n{{/is_alert}}\n\n{{#is_recovery}}\n\n- Don't forget  to close an open incidents on status.jenkins.io\n- Have you update our [runbooks][https://github.com/jenkins-infra/runbooks]? \n\n{{/is_recovery}}\n\nNotify: @pagerduty"
-  query               = "avg(last_30m):avg:jenkins.package.available{*} by {package} <= 0"
+  query               = "avg(last_30m):avg:jenkins.package.available{(NOT package:opensuse) AND (NOT package:redhat )} by {package} <= 0"
   notify_audit        = false
   timeout_h           = 0
   no_data_timeframe   = 60
