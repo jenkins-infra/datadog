1f4e8f1da9cc5c0b83b118548c388457d35061de
Jaideep <79799016+jayfranco999@users.noreply.github.com>
2026-03-30 14:35:13 +0530

    feat(monitors): add build report staleness monitor (#332)

---
+++ monitor-build-reports.tf
@@ -0,0 +1,46 @@
+# Monitor build report staleness from builds.reports.jenkins.io
+#
+# Metrics collected by the buildReportStaleness custom AgentCheck
+# deployed via kubernetes-management/config/datadog_confd_checksd.yaml
+#
+# Ref: https://github.com/jenkins-infra/helpdesk/issues/2843
+
+resource "datadog_monitor" "build_report_stale" {
+  name    = "Build report {{ job.name }} on {{ controller.name }} is stale"
+  type    = "query alert"
+  message = <<-EOT
+    {{#is_alert}}
+
+    - The build report for {{ job.name }} on {{ controller.name }} has not been updated in over {{ jenkins.build_report.threshold_in_hours }} hour(s)
+    - Check the job on the private controller {{ controller.name }}
+    - Job URL: https://{{ controller.name }}/job/{{ job.name }}
+    - Report: https://builds.reports.jenkins.io/build_status_reports/{{ controller.name }}/{{ job.name }}/status.json
+
+    - https://github.com/jenkins-infra/helpdesk/issues/2843
+
+    {{/is_alert}}
+
+    {{#is_recovery}}
+
+    - Build report for {{ job.name }} on {{ controller.name }} is fresh again
+
+    {{/is_recovery}}
+
+    Notify: @pagerduty
+  EOT
+
+  query               = "avg(last_1h):avg:jenkins.build_report.stale{*} by {controller,job} > 0"
+  notify_audit        = false
+  timeout_h           = 0
+  no_data_timeframe   = 120
+  renotify_interval   = 60
+  new_group_delay     = 300
+  require_full_window = false
+  draft_status        = "draft"
+
+  monitor_thresholds {
+    critical          = 0
+  }
+
+  tags = ["terraformed:true", "*"]
+}
