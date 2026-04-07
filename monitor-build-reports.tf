e09af723d291c9f66eb60834a6ef9ac0ad907762
Jaideep <79799016+jayfranco999@users.noreply.github.com>
2026-04-07 19:38:46 +0530

    fix(monitor-build-reports): externalize job config and fix staleness query (#336)
    * fix(monitor-build-reports): externalize job config and fix staleness query
    
    Signed-off-by: jayfranco999 <jaideepfrancis999@gmail.com>
    
    * fixup
    
    Signed-off-by: jayfranco999 <jaideepfrancis999@gmail.com>
    
    * fixup
    
    Signed-off-by: jayfranco999 <jaideepfrancis999@gmail.com>
    
    * Update build-report-jobs.yaml
    
    ---------
    
    Signed-off-by: jayfranco999 <jaideepfrancis999@gmail.com>
    Co-authored-by: Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>

--- monitor-build-reports.tf
+++ monitor-build-reports.tf
@@ -5,13 +5,20 @@
 #
 # Ref: https://github.com/jenkins-infra/helpdesk/issues/2843
 
+locals {
+  build_report_jobs = yamldecode(file("${path.module}/build-report-jobs.yaml")).build_report_jobs
+}
+
 resource "datadog_monitor" "build_report_stale" {
-  name    = "Build report {{ job.name }} on {{ controller.name }} is stale"
-  type    = "query alert"
+  for_each = local.build_report_jobs
+
+  name = "Build report ${each.value.job} on ${each.value.controller} is stale"
+  type = "query alert"
+
   message = <<-EOT
     {{#is_alert}}
 
-    - The build report for {{ job.name }} on {{ controller.name }} has not been updated in over {{ jenkins.build_report.threshold_in_hours }} hour(s)
+    - The build report for {{ job.name }} on {{ controller.name }} has not been updated in over {{ threshold_in_hours.name }} hour(s)
     - Check the job on the private controller {{ controller.name }}
     - Job URL: https://{{ controller.name }}/job/{{ job.name }}
     - Report: https://builds.reports.jenkins.io/build_status_reports/{{ controller.name }}/{{ job.name }}/status.json
@@ -29,17 +36,18 @@ resource "datadog_monitor" "build_report_stale" {
     Notify: @pagerduty
   EOT
 
-  query               = "avg(last_1h):avg:jenkins.build_report.stale{*} by {controller,job} > 0"
+# Alert if any check in the last 5m reports stale (>=1) for this specific controller+job
+  query               = "max(last_5m):avg:jenkins.build_report.stale{controller:${each.value.controller},job:${each.value.job}} >= 1"
   notify_audit        = false
   timeout_h           = 0
   no_data_timeframe   = 120
   renotify_interval   = 60
-  new_group_delay     = 300
+  # new_group_delay     = 300
   require_full_window = false
   draft_status        = "draft"
 
   monitor_thresholds {
-    critical          = 0
+    critical = 1
   }
 
   tags = ["terraformed:true", "*"]
